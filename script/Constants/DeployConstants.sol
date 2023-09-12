5434ab4060c752a3fe542b3e1912bf526053cf3a
j-vp <jasper@arcadia.finance>
2023-09-12 16:24:43 +0200

    🩹 fix usdc address to native usdc

--- script/Constants/DeployConstants.sol
+++ script/Constants/DeployConstants.sol
@@ -6,7 +6,7 @@ pragma solidity 0.8.19;
 
 library DeployAddresses {
     address public constant eth_base = 0x4200000000000000000000000000000000000006;
-    address public constant usdc_base = 0xd9aAEc86B65D86f6A7B5B1b0c42FFA531710b6CA;
+    address public constant usdc_base = 0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913;
 
     address public constant treasury_base = 0xBED53dA1718aDD7E31f42a138231c7783829A3f4; //to do: change for gnosis
 }
