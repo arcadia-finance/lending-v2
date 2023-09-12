aa8fa07bd2f21c2cae0483fd2b1b9d6a3220bb90
j-vp <jasper@arcadia.finance>
2023-09-12 16:30:07 +0200

    ✏️ todo tag

--- script/Constants/DeployConstants.sol
+++ script/Constants/DeployConstants.sol
@@ -8,5 +8,5 @@ library DeployAddresses {
     address public constant eth_base = 0x4200000000000000000000000000000000000006;
     address public constant usdc_base = 0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913;
 
-    address public constant treasury_base = 0xBED53dA1718aDD7E31f42a138231c7783829A3f4; //to do: change for gnosis
+    address public constant treasury_base = 0xBED53dA1718aDD7E31f42a138231c7783829A3f4; //todo: change for gnosis
 }
