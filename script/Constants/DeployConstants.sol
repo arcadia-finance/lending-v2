3a52502854f23c30e80dae06e5db54bd737f22c2
j-vp <jasper@arcadia.finance>
2023-09-12 09:10:22 +0200

    🚀 deploy lending v2

---
+++ script/Constants/DeployConstants.sol
@@ -0,0 +1,12 @@
+/**
+ * Created by Pragma Labs
+ * SPDX-License-Identifier: BUSL-1.1
+ */
+pragma solidity 0.8.19;
+
+library DeployAddresses {
+    address public constant eth_base = 0x4200000000000000000000000000000000000006;
+    address public constant usdc_base = 0xd9aAEc86B65D86f6A7B5B1b0c42FFA531710b6CA;
+
+    address public constant treasury_base = 0xBED53dA1718aDD7E31f42a138231c7783829A3f4; //to do: change for gnosis
+}
