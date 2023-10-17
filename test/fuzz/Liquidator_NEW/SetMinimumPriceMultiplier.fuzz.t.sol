d3e2169aa978c9bfb68ae2f0bdd35a10c58082fb
zekiblue <zeki@arcadia.finance>
2023-10-17 14:25:52 +0200

    :white_check_mark: Add parameter tests to new tests

---
+++ test/fuzz/Liquidator_NEW/SetMinimumPriceMultiplier.fuzz.t.sol
@@ -0,0 +1,50 @@
+/**
+ * Created by Pragma Labs
+ * SPDX-License-Identifier: BUSL-1.1
+ */
+pragma solidity 0.8.19;
+
+import { Liquidator_Fuzz_Test } from "./_Liquidator.fuzz.t.sol";
+
+/**
+ * @notice Fuzz tests for the function "setMinimumPriceMultiplier" of contract "Liquidator".
+ */
+contract SetMinimumPriceMultiplier_Liquidator_Fuzz_Test is Liquidator_Fuzz_Test_NEW {
+    /* ///////////////////////////////////////////////////////////////
+                              SETUP
+    /////////////////////////////////////////////////////////////// */
+
+    function setUp() public override {
+        Liquidator_Fuzz_Test_NEW.setUp();
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                              TESTS
+    //////////////////////////////////////////////////////////////*/
+    function testFuzz_Revert_setMinimumPriceMultiplier_tooHigh(uint8 priceMultiplier) public {
+        // Preprocess: limit the fuzzing to acceptable levels
+        vm.assume(priceMultiplier >= 91);
+
+        // Given When Then: a owner attempts to set the minimum price multiplier, but it is not in the limits
+        vm.startPrank(users.creatorAddress);
+        vm.expectRevert("LQ_SMPM: multiplier too high");
+        liquidator_new.setMinimumPriceMultiplier(priceMultiplier);
+        vm.stopPrank();
+    }
+
+    function testFuzz_Success_setMinimumPriceMultiplier(uint8 priceMultiplier) public {
+        // Preprocess: limit the fuzzing to acceptable levels
+        vm.assume(priceMultiplier < 91);
+        // Given: the owner is the users.creatorAddress
+
+        vm.startPrank(users.creatorAddress);
+        // When: the owner sets the minimum price multiplier
+        vm.expectEmit(true, true, true, true);
+        emit MinimumPriceMultiplierSet(priceMultiplier);
+        liquidator_new.setMinimumPriceMultiplier(priceMultiplier);
+        vm.stopPrank();
+
+        // Then: multiplier sets correctly
+        assertEq(liquidator_new.getMinPriceMultiplier(), priceMultiplier);
+    }
+}
