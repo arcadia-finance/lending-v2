a0862a1f2d03069396f7c58606584a12d4fba7b7
zekiblue <zeki@arcadia.finance>
2023-10-20 13:23:57 +0200

    :twisted_rightwards_arrows: Take the basic of liquidator

---
+++ test/fuzz/Liquidator_NEW/SetAuctionCurveParameters.fuzz.t.sol
@@ -0,0 +1,144 @@
+/**
+ * Created by Pragma Labs
+ * SPDX-License-Identifier: BUSL-1.1
+ */
+pragma solidity 0.8.19;
+
+import { Liquidator_Fuzz_Test_NEW } from "./_Liquidator.fuzz.t.sol";
+
+import { LogExpMath } from "../../../src/libraries/LogExpMath.sol";
+
+/**
+ * @notice Fuzz tests for the function "setAuctionCurveParameters" of contract "Liquidator".
+ */
+contract SetAuctionCurveParameters_Liquidator_Fuzz_Test is Liquidator_Fuzz_Test_NEW {
+    /* ///////////////////////////////////////////////////////////////
+                              SETUP
+    /////////////////////////////////////////////////////////////// */
+
+    function setUp() public override {
+        Liquidator_Fuzz_Test_NEW.setUp();
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                              TESTS
+    //////////////////////////////////////////////////////////////*/
+    function testFuzz_Revert_setAuctionCurveParameters_NonOwner(
+        address unprivilegedAddress_,
+        uint16 halfLifeTime,
+        uint16 cutoffTime
+    ) public {
+        vm.assume(unprivilegedAddress_ != users.creatorAddress);
+
+        vm.startPrank(unprivilegedAddress_);
+        vm.expectRevert("UNAUTHORIZED");
+        liquidator_new.setAuctionCurveParameters(halfLifeTime, cutoffTime);
+        vm.stopPrank();
+    }
+
+    function testFuzz_Revert_setAuctionCurveParameters_BaseTooHigh(uint16 halfLifeTime, uint16 cutoffTime) public {
+        // Preprocess: limit the fuzzing to acceptable levels
+        vm.assume(halfLifeTime > 8 * 60 * 60);
+
+        // Given When Then: a owner attempts to set the discount rate, but it is not in the limits
+        vm.startPrank(users.creatorAddress);
+        vm.expectRevert("LQ_SACP: halfLifeTime too high");
+        liquidator_new.setAuctionCurveParameters(halfLifeTime, cutoffTime);
+        vm.stopPrank();
+    }
+
+    function testFuzz_Revert_setAuctionCurveParameters_BaseTooLow(uint16 halfLifeTime, uint16 cutoffTime) public {
+        // Preprocess: limit the fuzzing to acceptable levels
+        vm.assume(halfLifeTime < 2 * 60);
+
+        // Given When Then: a owner attempts to set the discount rate, but it is not in the limits
+        vm.startPrank(users.creatorAddress);
+        vm.expectRevert("LQ_SACP: halfLifeTime too low");
+        liquidator_new.setAuctionCurveParameters(halfLifeTime, cutoffTime);
+        vm.stopPrank();
+    }
+
+    function testFuzz_Revert_setAuctionCurveParameters_AuctionCutoffTimeTooHigh(uint16 halfLifeTime, uint16 cutoffTime)
+        public
+    {
+        // Preprocess: limit the fuzzing to acceptable levels
+        vm.assume(halfLifeTime > 2 * 60);
+        vm.assume(halfLifeTime < 8 * 60 * 60);
+
+        vm.assume(cutoffTime > 18 * 60 * 60);
+
+        // Given When Then: a owner attempts to set the max auction time, but it is not in the limits
+        vm.startPrank(users.creatorAddress);
+        vm.expectRevert("LQ_SACP: cutoff too high");
+        liquidator_new.setAuctionCurveParameters(halfLifeTime, cutoffTime);
+        vm.stopPrank();
+    }
+
+    function testFuzz_Revert_setAuctionCurveParameters_AuctionCutoffTimeTooLow(uint16 halfLifeTime, uint16 cutoffTime)
+        public
+    {
+        // Preprocess: limit the fuzzing to acceptable levels
+        vm.assume(halfLifeTime > 2 * 60);
+        vm.assume(halfLifeTime < 8 * 60 * 60);
+
+        vm.assume(cutoffTime < 1 * 60 * 60);
+
+        // Given When Then: a owner attempts to set the max auction time, but it is not in the limits
+        vm.startPrank(users.creatorAddress);
+        vm.expectRevert("LQ_SACP: cutoff too low");
+        liquidator_new.setAuctionCurveParameters(halfLifeTime, cutoffTime);
+        vm.stopPrank();
+    }
+
+    function testFuzz_Revert_setAuctionCurveParameters_PowerFunctionReverts(uint8 halfLifeTime, uint16 cutoffTime)
+        public
+    {
+        // Preprocess: limit the fuzzing to acceptable levels
+        vm.assume(halfLifeTime > 2 * 60);
+        vm.assume(halfLifeTime < 15 * 60);
+        vm.assume(cutoffTime > 10 * 60 * 60);
+        vm.assume(cutoffTime < 18 * 60 * 60);
+
+        vm.startPrank(users.creatorAddress);
+        vm.expectRevert();
+        liquidator_new.setAuctionCurveParameters(halfLifeTime, cutoffTime);
+        vm.stopPrank();
+    }
+
+    function testFuzz_Success_setAuctionCurveParameters_Base(uint16 halfLifeTime, uint16 cutoffTime) public {
+        // Preprocess: limit the fuzzing to acceptable levels
+        vm.assume(halfLifeTime > 2 * 60);
+        vm.assume(halfLifeTime < 8 * 60 * 60);
+        vm.assume(cutoffTime > 1 * 60 * 60);
+        vm.assume(cutoffTime < 2 * 60 * 60);
+
+        uint256 expectedBase = 1e18 * 1e18 / LogExpMath.pow(2 * 1e18, uint256(1e18 / halfLifeTime));
+
+        // Given: the owner is the users.creatorAddress
+        vm.startPrank(users.creatorAddress);
+        // When: the owner sets the discount rate
+        vm.expectEmit(true, true, true, true);
+        emit AuctionCurveParametersSet(uint64(expectedBase), cutoffTime);
+        liquidator_new.setAuctionCurveParameters(halfLifeTime, cutoffTime);
+        vm.stopPrank();
+
+        // Then: the discount rate is correctly set
+        assertEq(liquidator_new.getBase(), expectedBase);
+    }
+
+    function testFuzz_Success_setAuctionCurveParameters_cutoffTime(uint16 halfLifeTime, uint16 cutoffTime) public {
+        // Preprocess: limit the fuzzing to acceptable levels
+        vm.assume(halfLifeTime > 1 * 60 * 60);
+        vm.assume(halfLifeTime < 8 * 60 * 60);
+        vm.assume(cutoffTime > 1 * 60 * 60);
+        vm.assume(cutoffTime < 8 * 60 * 60);
+
+        // Given: the owner is the users.creatorAddress
+        vm.prank(users.creatorAddress);
+        // When: the owner sets the max auction time
+        liquidator_new.setAuctionCurveParameters(halfLifeTime, cutoffTime);
+
+        // Then: the max auction time is set
+        assertEq(liquidator_new.getCutoffTime(), cutoffTime);
+    }
+}
---
+++ test/fuzz/Liquidator_NEW/SetWeights.fuzz.t.sol
@@ -0,0 +1,58 @@
+/**
+ * Created by Pragma Labs
+ * SPDX-License-Identifier: BUSL-1.1
+ */
+pragma solidity 0.8.19;
+
+import { Liquidator_Fuzz_Test_NEW } from "./_Liquidator.fuzz.t.sol";
+
+/**
+ * @notice Fuzz tests for the function "setWeights" of contract "Liquidator".
+ */
+contract SetWeights_Liquidator_Fuzz_Test is Liquidator_Fuzz_Test_NEW {
+    /* ///////////////////////////////////////////////////////////////
+                              SETUP
+    /////////////////////////////////////////////////////////////// */
+
+    function setUp() public override {
+        Liquidator_Fuzz_Test_NEW.setUp();
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                              TESTS
+    //////////////////////////////////////////////////////////////*/
+    function testFuzz_Revert_setWeights_NonOwner(
+        address unprivilegedAddress_,
+        uint8 initiatorRewardWeight,
+        uint8 penaltyWeight
+    ) public {
+        vm.assume(unprivilegedAddress_ != users.creatorAddress);
+
+        vm.startPrank(unprivilegedAddress_);
+        vm.expectRevert("UNAUTHORIZED");
+        liquidator_new.setWeights(initiatorRewardWeight, penaltyWeight);
+        vm.stopPrank();
+    }
+
+    function testFuzz_Revert_setWeights_WeightsTooHigh(uint8 initiatorRewardWeight, uint8 penaltyWeight) public {
+        vm.assume(uint16(initiatorRewardWeight) + penaltyWeight > 11);
+
+        vm.startPrank(users.creatorAddress);
+        vm.expectRevert("LQ_SW: Weights Too High");
+        liquidator_new.setWeights(initiatorRewardWeight, penaltyWeight);
+        vm.stopPrank();
+    }
+
+    function testFuzz_Success_setWeights(uint8 initiatorRewardWeight, uint8 penaltyWeight) public {
+        vm.assume(uint16(initiatorRewardWeight) + penaltyWeight <= 11);
+
+        vm.startPrank(users.creatorAddress);
+        vm.expectEmit(true, true, true, true);
+        emit WeightsSet(initiatorRewardWeight, penaltyWeight);
+        liquidator_new.setWeights(initiatorRewardWeight, penaltyWeight);
+        vm.stopPrank();
+
+        assertEq(liquidator_new.getPenaltyWeight(), penaltyWeight);
+        assertEq(liquidator_new.getInitiatorRewardWeight(), initiatorRewardWeight);
+    }
+}
