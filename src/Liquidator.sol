5c7a0be047ecfa32c9c35db371a4422e24acd707
zeki <zeki@arcadia.finance>
2023-11-24 17:06:05 +0100

    :recycle: Update src/Liquidator.sol
    Co-authored-by: Thomas-Smets <43761140+Thomas-Smets@users.noreply.github.com>

--- src/Liquidator.sol
+++ src/Liquidator.sol
@@ -330,7 +330,9 @@ contract Liquidator is Owned, ILiquidator {
      * base: defines how fast the exponential curve decreases (18 decimals precision).
      * t: time passed since start auction (in seconds, 18 decimals precision).
      * @dev LogExpMath was made in solidity 0.7, where operations were unchecked.
-     * @dev Calculating the bid price after cutoffTime becomes unstable. When the timePassed increasing the LogExpMath
+     * @dev Successful bids are only guaranteed until the cutoffTime.
+     * Calculating the bid price after the cutoffTime will revert at some point (due to too low precision of the LogExpMath library).
+     * Take this into account for bids done after the cutoffTime.
      * might revert. Be aware the bidding after cutoffTime might revert due to this.
      */
     function _calculateBidPrice(AuctionInformation storage auctionInformation_, uint256 totalShare)
