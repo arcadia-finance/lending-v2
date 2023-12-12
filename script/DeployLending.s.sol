225a1e719e31eba25a25a759c684497bffaec139
j-vp <jasper@arcadia.finance>
2023-12-12 19:46:44 +0100

    🚀 adjust deploy script & test deployed state

--- script/DeployLending.s.sol
+++ script/DeployLending.s.sol
@@ -12,10 +12,9 @@ import { DeployAddresses } from "./Constants/DeployConstants.sol";
 import { Factory } from "../lib/accounts-v2/src/Factory.sol";
 import { Liquidator } from "../src/Liquidator.sol";
 
-import { ERC20, DebtToken } from "../src/DebtToken.sol";
+import { ERC20 } from "../src/DebtToken.sol";
 import { LendingPool } from "../src/LendingPool.sol";
 import { Tranche } from "../src/Tranche.sol";
-import { Creditor } from "../lib/accounts-v2/src/abstracts/Creditor.sol";
 
 contract ArcadiaLendingDeployment is Test {
     Factory public factory;
@@ -40,6 +39,8 @@ contract ArcadiaLendingDeployment is Test {
         vm.startBroadcast(deployerPrivateKey);
 
         factory = new Factory();
+        factory.changeGuardian(vm.addr(deployerPrivateKey));
+
         liquidator = new Liquidator();
 
         pool_weth = new LendingPool(
@@ -51,13 +52,12 @@ contract ArcadiaLendingDeployment is Test {
         );
         srTranche_weth = new Tranche(address(pool_weth), "Senior", "sr");
 
-        pool_weth.setOriginationFee(10);
-        pool_weth.setLiquidationParameters(100, 500, 50, 3 * 10 ** 18, 3 * 10 ** 18);
+        pool_weth.setLiquidationParameters(100, 500, 50, 2 * 10 ** 18, 2 * 10 ** 18);
         pool_weth.setFixedLiquidationCost(0.002 * 10 ** 18);
-        pool_weth.addTranche(address(srTranche_weth), 85, 10);
-        pool_weth.setTreasuryWeights(15, 90);
+        pool_weth.addTranche(address(srTranche_weth), 100, 100);
         pool_weth.setInterestParameters(15_000_000_000_000_000, 70_000_000_000_000_000, 1_250_000_000_000_000_000, 7000);
         pool_weth.changeGuardian(vm.addr(deployerPrivateKey));
+        pool_weth.setRiskManager(vm.addr(deployerPrivateKey));
 
         pool_usdc = new LendingPool(
             vm.addr(deployerPrivateKey),
@@ -68,14 +68,33 @@ contract ArcadiaLendingDeployment is Test {
         );
         srTranche_usdc = new Tranche(address(pool_usdc), "Senior", "sr");
 
-        pool_usdc.setOriginationFee(10);
-        pool_weth.setLiquidationParameters(100, 500, 50, 5000 * 10 ** 6, 5000 * 10 ** 6);
+        pool_usdc.setLiquidationParameters(100, 500, 50, 5000 * 10 ** 6, 5000 * 10 ** 6);
         pool_usdc.setFixedLiquidationCost(2 * 10 ** 6);
-        pool_usdc.addTranche(address(srTranche_usdc), 85, 10);
-        pool_usdc.setTreasuryWeights(15, 90);
+        pool_usdc.addTranche(address(srTranche_usdc), 100, 100);
         pool_usdc.setInterestParameters(10_000_000_000_000_000, 55_000_000_000_000_000, 1_000_000_000_000_000_000, 8000);
         pool_usdc.changeGuardian(vm.addr(deployerPrivateKey));
 
         vm.stopBroadcast();
+
+        test();
+    }
+
+    function test() public view {
+        address deployerAddress = vm.addr(vm.envUint("PRIVATE_KEY_DEPLOYER_BASE"));
+        assert(pool_usdc.guardian() == deployerAddress);
+        assert(pool_weth.guardian() == deployerAddress);
+
+        assert(pool_usdc.asset() == usdc);
+        assert(pool_weth.asset() == weth);
+
+        assert(pool_usdc.owner() == deployerAddress);
+        assert(pool_weth.owner() == deployerAddress);
+        assert(pool_usdc.riskManager() == deployerAddress);
+        assert(pool_weth.riskManager() == deployerAddress);
+
+        assert(factory.owner() == deployerAddress);
+        assert(factory.guardian() == deployerAddress);
+
+        assert(liquidator.owner() == deployerAddress);
     }
 }
